5e8fd7dfc9693af9127620cfada71fe3dc3b1cdc
Lingye <84624193+WENSHAO521@users.noreply.github.com>
2026-08-21 20:13:27 +0900

    ui: mount professional reader action menu

--- mail-vue/src/App.vue
+++ mail-vue/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <el-config-provider :locale="settingStore.lang === 'zh' ? zhCn : null">
     <router-view />
+    <ReaderActionMenu />
   </el-config-provider>
 </template>
 <script setup>
 import { useI18n } from "vue-i18n";
 import { watch } from "vue";
 import {useSettingStore} from "@/store/setting.js";
+import ReaderActionMenu from '@/components/reader-action-menu/index.vue'
 const settingStore = useSettingStore()
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import('@/icons/index.js')
